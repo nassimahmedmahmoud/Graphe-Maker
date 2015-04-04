63ae9c02ed2d017f392d1038d2be892eb4af293a
Airenn <nassim.ahmed.mahmoud@gmail.com>
2015-04-04 16:15:50 +0200

    BugFix + maj Dijkstra
    Fonctionnalité :
    #1 : ajout d’une animation pour tracer le chemin le plus court
    BugFix:
    #1 : Maintenant notre Dijkstra fonctionne pour les graphe orientés.
    #2 : le Dmin renvoie les bonnes valeurs

--- Graph Maker/src/dialogue/JPanelDijkstra.java
+++ Graph Maker/src/dialogue/JPanelDijkstra.java
@@ -3,20 +3,45 @@ package dialogue;
 import graphe.*;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.geom.*;
 import java.util.*;
-
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class JPanelDijkstra extends JPanel{
 
 	private static final long serialVersionUID = 1L;
+	private static final int ANIM_DELAY = 0; // 50ms == 20fps
+	private static final float ARC_DASH_LENGTH = 9;
+	private static final float DASH_OFFSET_SUM = 0.5f;
+	
 	private GrapheView gv;
+	private float dashOffset = 0;
+	private Timer animTimer;
 
 	public JPanelDijkstra(LayoutManager layout,GrapheView gv)
 	{
 		super(layout);
 		this.gv = gv;
+		this.animTimer = new Timer(ANIM_DELAY, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JPanelDijkstra.this.repaint();
+			}
+		});
+		this.animTimer.setRepeats(true);
+		this.animationStart();
+	}
+	
+	public void animationStart()
+	{
+		this.animTimer.start();
+	}
+	
+	public void animationStop()
+	{
+		this.animTimer.stop();
 	}
 
 	public void paintComponent(Graphics g)
@@ -25,22 +50,21 @@ public class JPanelDijkstra extends JPanel{
 		((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		gv.getDijkstra().algorithmDijkstra();
 		for(Arc a : gv.getGraphe().getArcs()){
-			this.drawLines(g,a,Color.BLACK);
-		}
-		
-		for(Sommet s : gv.getGraphe().getSommets()){
-			drawNode(s,g,gv.getGraphe().getTailleSommet(),Color.WHITE);
-		}
-		if(gv.getDijkstra().getSource()!=null){
-			drawNode(gv.getDijkstra().getSource(), g,gv.getGraphe().getTailleSommet(), Color.CYAN);
+			this.drawLines(g,a,Color.BLACK, false);
 		}
 		if(gv.getDijkstra().getArrivee()!=null){
 			ArrayList<Arc> tab = gv.getDijkstra().distanceSourceArc();
 			for(Arc arc : tab)
-				this.drawLines(g,arc,Color.CYAN);
-			drawNode(gv.getDijkstra().getArrivee(), g,gv.getGraphe().getTailleSommet(), Color.RED);
+				this.drawLines(g,arc,Color.CYAN, true);
 		}
-		System.out.println(gv.getDijkstra().getSource());
+		for(Sommet s : gv.getGraphe().getSommets()){
+			drawNode(s,g,gv.getGraphe().getTailleSommet(),Color.WHITE);
+		}
+		if(gv.getDijkstra().getSource()!=null)
+			drawNode(gv.getDijkstra().getSource(), g,gv.getGraphe().getTailleSommet(), Color.CYAN);
+		if(gv.getDijkstra().getArrivee()!=null)
+			drawNode(gv.getDijkstra().getArrivee(), g,gv.getGraphe().getTailleSommet(), Color.RED);
+		dashOffset += DASH_OFFSET_SUM;
 	} 
 
 	void drawNode(Sommet s,Graphics g,int diametre,Color color)
@@ -55,17 +79,54 @@ public class JPanelDijkstra extends JPanel{
 		g.setColor(Color.black);
 	}
 
-	public void drawLines(Graphics g, Arc a,Color color)
+	public void drawLines(Graphics g, Arc a,Color color, boolean dashed)
 	{
-		g.setColor(color);
+		Graphics gctx = dashed ? (Graphics2D)g.create() : g;
+		
+		gctx.setColor(color);
 		int x1=a.getOrigine().getPosX();
 		int x2=a.getArrivee().getPosX();
 		int y1=a.getOrigine().getPosY();
 		int y2=a.getArrivee().getPosY();
 		int ctrlx=a.getCentre_posX();
 		int ctrly=a.getCentre_posY();
-		g.drawLine(x1, y1, x2, y2);
+		Path2D p = new Path2D.Double();
+		if (dashed) {
+			Stroke s = new BasicStroke(3, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 0, new float[]{ARC_DASH_LENGTH}, dashOffset);
+			((Graphics2D)gctx).setStroke(s);
+		}
+		if(gv.getGraphe().isType()){
+			drawArrow(g,x1,y1,x2,y2);
+			if(a.boucleMemeSommet()){
+				//cc.setCurve(x1, y1, 0,0,0,0, x2, y2);
+				p.moveTo(x1, y1);
+				p.curveTo(x1, y1,x1/2, y2/2, x2, y2);
+				((Graphics2D) gctx).draw(p);
+			}
+		}
+		gctx.drawLine(x1, y1, x2, y2);
+		
+		if (dashed) {
+			gctx.dispose(); // on tej' la copy du contexte
+			gctx = g;
+		}
+		
 		g.drawString(a.getNom(),ctrlx,ctrly);
 		g.setColor(Color.BLACK);
 	}  
+	void drawArrow(Graphics g1, int x1, int y1, int x2, int y2) {
+		Graphics2D g = (Graphics2D) g1.create();
+		double dx = x2 - x1, dy = y2 - y1;
+		double angle = Math.atan2(dy, dx);
+		int len = (int) Math.sqrt(dx*dx + dy*dy);
+		AffineTransform at =affineTransforme(x1,y1,x2,y2);
+		at.concatenate(AffineTransform.getRotateInstance(angle));
+		g.transform(at);
+		g.fillPolygon(new int[] {len/2, len/2-JPanelGraphe.ARR_SIZE, len/2-JPanelGraphe.ARR_SIZE, len/2},
+				new int[] {0, -JPanelGraphe.ARR_SIZE, JPanelGraphe.ARR_SIZE, 0}, 4);
+	}
+	public AffineTransform affineTransforme(int x1, int y1, int x2, int y2){
+		AffineTransform at = AffineTransform.getTranslateInstance(x1, y1);
+		return at;
+	}
 }
--- Graph Maker/src/graphe/Dijkstra.java
+++ Graph Maker/src/graphe/Dijkstra.java
@@ -30,7 +30,6 @@ public class Dijkstra {
 		if (ui && source!=null) {
 			int indexSource = graphe.getArcinit().indexOf(source);
 			dijkstraAlgorithm(indexSource);
-			dmin[0]=dmin[1]+valArc(graphe.getArcinit().get(0), graphe.getArcinit().get(1));
 		}
 	}
 
@@ -52,20 +51,19 @@ public class Dijkstra {
 	public void dijkstraAlgorithm(int index) {
 		do {
 			int[] tabVoisins = graphe.tabVoisins(index);
-
+			String s = "indexDep :"+index;
 			index = smallestValue(tabVoisins);
+			System.out.println(s+" index 1  : "+index+"\t");
 			if (index == -1) {
 				index = smallestValue();
 			}
-			if (index == -1) {
+			if (index == -1){
 				index = marquageFirstIndex();
 			}
-			System.out.print("dmin : ");
-			for(int i=0;i<dmin.length;i++){
-				System.out.print(dmin[i]+"|");
-			}
+			if(index!=-1)
+				dminAJour(index);
+			marquage[index]=true;
 			System.out.println();
-			dminAJour(index);
 		}while(!isMarqued());
 	}
 
@@ -91,9 +89,9 @@ public class Dijkstra {
 		}
 		int val = -1;
 		for (int i = 0; i < tabVoisins.length; i++) {
-			if (smallest > dmin[tabVoisins[i]] && !marquage[i]) {
+			if ((smallest > dmin[tabVoisins[i]]) && !marquage[tabVoisins[i]]) {
 				smallest = dmin[tabVoisins[i]];
-				val = i;
+				val = tabVoisins[i];
 			}
 		}
 		return val;
@@ -110,6 +108,12 @@ public class Dijkstra {
 
 	public void dminAJour(int sommetMarque) {
 		int[] tabVoisins = graphe.tabVoisins(sommetMarque);
+		System.out.println("sommet Marque: "+sommetMarque+" tabvoisins : ");
+		for(int i=0;i<tabVoisins.length;i++){
+			System.out.print(tabVoisins[i]+"\t");
+		}
+		System.out.println("");
+		
 		if (tabVoisins.length == 0) {
 			return;
 		}
@@ -120,7 +124,6 @@ public class Dijkstra {
 				sommetProche[tabVoisins[i]] = sommetMarque;
 			}
 		}
-		marquage[sommetMarque]=true;
 	}
 
 	public boolean isMarqued() {
@@ -161,19 +164,13 @@ public class Dijkstra {
 
 	public ArrayList<Sommet> distanceSource(Sommet arrive){
 		ArrayList<Sommet> tabFinal = new ArrayList<Sommet>();
-
 		int indexLocal = graphe.getArcinit().indexOf(arrive);
-		int i=1;
 		tabFinal.add(graphe.getArcinit().get(indexLocal));
 		while(indexLocal!=-1){
 			if(indexLocal!=-1){
 				tabFinal.add(graphe.getArcinit().get(indexLocal));
 				indexLocal=sommetProche[indexLocal];
 			}
-		}
-		System.out.print("tabFinal : ");
-		for (int j = 0; j < tabFinal.size(); j++) {
-			System.out.print("|"+tabFinal.get(j)+"|");
 		}
 		return tabFinal;
 	}
@@ -185,20 +182,34 @@ public class Dijkstra {
 			Arc a=null;
 			if(sommetProche[indexLocal]!=-1)
 				a= graphe.arcaPartirSommets(graphe.getArcinit().get(indexLocal),graphe.getArcinit().get(sommetProche[indexLocal]));
-			if(a!=null){
-				System.out.println("Arc : "+a.getNom());
+			if(a!=null)
 				tabFinal.add(a);
-			}
 			indexLocal=sommetProche[indexLocal];
-			System.out.println("indexLocal : "+indexLocal);
-		}
-		System.out.print("tabFinal : ");
-		for (int j = 0; j < tabFinal.size(); j++) {
-			System.out.print("|"+tabFinal.get(j)+"|");
 		}
 		return tabFinal;
 	}
-
+	
+	public int[] tabVoisins(int index)
+	{
+		int[]tab=new int[graphe.getSommets().get(index).getArcs().size()];
+		for(int i = 0; i < graphe.getSommets().get(index).getArcs().size(); i++){
+			int voisinArrivee=graphe.getSommets().indexOf(graphe.getSommets().get(index).getArcs().get(i).getArrivee());
+			if(voisinArrivee!=index)
+				tab[i]=voisinArrivee;
+			else if(!graphe.isType()){
+				voisinArrivee=graphe.getSommets().indexOf(graphe.getSommets().get(index).getArcs().get(i).getOrigine());
+				tab[i]=voisinArrivee;
+			}
+			else
+				tab[i]=voisinArrivee;			
+		}
+		return tab;
+	}
+	public boolean successeurPossible(){
+		boolean ui=false;
+		
+		return ui;
+	}
 	public int[] getDmin() {
 		return dmin;
 	}
@@ -247,7 +258,10 @@ public class Dijkstra {
 		}
 		s+="</tr><tr><td>Dmin</td>";
 		for(int i=0;i<dmin.length;i++){
-			s+="<td>"+dmin[i]+"</td>";
+			if(dmin[i]!=Integer.MAX_VALUE)
+				s+="<td>"+dmin[i]+"</td>";
+			else
+				s+="<td>Impossible</td>";
 		}
 		s+="</tr><tr><td>Sommets proches</td>";
 		for(int i=0;i<sommetProche.length;i++){

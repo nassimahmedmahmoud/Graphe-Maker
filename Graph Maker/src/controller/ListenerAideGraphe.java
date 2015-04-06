ea6bc4bb48d535b5293a1c6eaa763bb10523c88f
CcCvToa <Jean.Eudes.llas@gmail.Com>
2015-04-06 23:57:41 +0200

    Bugfix
    Bugfix :
    Fenêtre aide totalement opérationnelle.

--- Graph Maker/src/controller/ListenerAideGraphe.java
+++ Graph Maker/src/controller/ListenerAideGraphe.java
@@ -17,27 +17,21 @@ public class ListenerAideGraphe implements ActionListener
     {
        if(e.getSource() == gv.getPrec())
        {
-           gv.getTexte().setText("<html><body><p>" + gv.getA().assistance(gv.getA().getCurentpage())
-   				+ "</p></body></html>");
            if(gv.getA().getCurentpage() == 6)
            {
-               gv.getA().setCurentpage(gv.getA().getCurentpage()-1); 
-               gv.getPanbtn().remove(gv.getPrec());
-               gv.getPanbtn().add(gv.getPrec());
+               gv.getA().setCurentpage(gv.getA().getCurentpage()-1);
                gv.getPanbtn().add(gv.getSuiv());
            }
            else if(gv.getA().getCurentpage() != Aide.SOMMAIRE+1){
                gv.getA().setCurentpage(gv.getA().getCurentpage()-1);
-               gv.getPanbtn().remove(gv.getPrec());
-               gv.getPanbtn().remove(gv.getSuiv());
-               gv.getPanbtn().add(gv.getPrec());
-               gv.getPanbtn().add(gv.getSuiv());
            }
            else
            {
                gv.getA().setCurentpage(gv.getA().getCurentpage()-1);
-               gv.getPanbtn().remove(gv.getPrec());
+               gv.getPanbtn().remove(gv.getPrec());               
            }
+           gv.getTexte().setText("<html><body><p>" + gv.getA().assistance(gv.getA().getCurentpage())
+   				+ "</p></body></html>");
        }
        
        if(e.getSource() == gv.getSuiv())
